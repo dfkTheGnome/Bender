490f2e97fd851db5af7be470cfdb15866e97aed0
Mikel Burgos <drakognome@gmail.com>
2020-02-25 17:04:44 +0100

    Esqueleto

---
+++ PracticaBender/src/Bender.java
@@ -0,0 +1,16 @@
+
+public class Bender {
+	char[][] mapa;
+	Bender(String mapa){
+	      this.mapa = mapBuilder(mapa);
+	    }
+	char[][] mapBuilder(String m){
+		return null;
+	}
+	String run() {
+		return null;
+	}
+	String bestRun(){
+	        return null;
+	    }
+}
